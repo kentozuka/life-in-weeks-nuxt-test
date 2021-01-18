5ace7df46bac8ed883f1afd91671a7a4e5d48d0d
kento <kentozuka22@gmail.com>
2021-01-18 21:00:45 +0900

    forgot to un-gitignore dist

---
+++ _nuxt/static/1610971100/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flife-in-weeks-nuxt-test\u002F_nuxt\u002Fstatic\u002F1610971100",layout:"default",error:null,state:{birthday:a,username:a,counter:0},serverRendered:true,routePath:"\u002F",config:{content:{dbHash:"153a0ca7"}}}}(""));
